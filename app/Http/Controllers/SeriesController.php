d95eed2181e3bb91d4fcfd6d7c86f215388a1058
Luiz Silva <luiz.silva2@ciadeestagios.com.br>
2023-03-13 14:35:34 -0300

    Adicionando migration, model, salvando e listando séries no banco de dados

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -2,22 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Serie;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SeriesController extends Controller
 {
     public function index(Request $request)
     {
-        $series = [
-            'Punisher',
-            'Stranger Things',
-            'Rings of Power',
-        ];
+        //        $series = [
+        //            'Punisher',
+        //            'Stranger Things',
+        //            'Rings of Power',
+        //        ];
+        //        $series = DB::select('SELECT nome FROM series;');
+//        $series = Serie::all();
+        //Podemos também fazer queries mais complexas:
+        $series = Serie::query()->orderBy('nome')->get();
+        //        dd($series); // - para debugar (dump & die)
 
         return view('series.index')->with('series', $series);
     }
 
-    public function create(){
+    public function create()
+    {
         return view('series.create');
+    }
+
+    public function store(Request $request)
+    {
+        $nomeSerie = $request->input('nome');
+        $serie = new Serie();
+        $serie->nome = $nomeSerie;
+        $serie->save();
+        //        DB::insert('INSERT INTO series(nome) VALUES(?)', [$nomeSerie]);
+
+        return redirect('/series');
     }
 }
